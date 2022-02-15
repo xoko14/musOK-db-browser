f0f65b40a44ba598aced3047eb3a152f974efd85
xoko14 <xoquinperezb@gmail.com>
2022-02-15 20:24:47 +0100

    Initial commit

---
+++ src/main/kotlin/module-info.java
@@ -0,0 +1,16 @@
+module com.musok.musokdbbrowser {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires kotlin.stdlib;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.musok.musokdbbrowser to javafx.fxml;
+    exports com.musok.musokdbbrowser;
+}
