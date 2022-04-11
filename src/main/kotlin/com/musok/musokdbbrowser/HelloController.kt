c0f3815c3425d430c39296a6dee026eda643da52
xoko14 <xoquinperezb@gmail.com>
2022-04-11 19:12:34 +0200

    demodularize & custom scss styles

--- src/main/kotlin/com/musok/musokdbbrowser/HelloController.kt
+++ src/main/kotlin/com/musok/musokdbbrowser/HelloController.kt
@@ -2,6 +2,8 @@ package com.musok.musokdbbrowser
 
 import com.musok.musokdbbrowser.api.connection.Server
 import javafx.fxml.FXML
+import javafx.scene.control.Alert
+import javafx.scene.control.Alert.AlertType
 import javafx.scene.control.PasswordField
 import javafx.scene.control.TextField
 
@@ -21,6 +23,15 @@ class HelloController {
 
         val user = Server.getCurrentUser()
         println(user.username)
+        val alert = Alert(AlertType.INFORMATION)
+        val dialogPane = alert.dialogPane
+        dialogPane.stylesheets.add(
+            this::class.java.getResource("/theme/dark-theme.css")?.toExternalForm()
+        )
+        alert.title = "Log in correct"
+        alert.headerText = "Welcome ${user.username}"
+        alert.contentText = "User id: ${user.id}"
+        alert.showAndWait()
     }
 
 
