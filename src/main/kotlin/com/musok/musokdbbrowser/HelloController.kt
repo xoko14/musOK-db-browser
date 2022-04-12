27e0bad60070ba90c78d9c81a432fcd4c7f5012b
Xoquín Pérez Blanco <xoquinpb@wappingweb.com>
2022-04-12 11:43:29 +0200

    show main window on login

--- src/main/kotlin/com/musok/musokdbbrowser/HelloController.kt
+++ src/main/kotlin/com/musok/musokdbbrowser/HelloController.kt
@@ -2,10 +2,14 @@ package com.musok.musokdbbrowser
 
 import com.musok.musokdbbrowser.api.connection.Server
 import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
 import javafx.scene.control.Alert
 import javafx.scene.control.Alert.AlertType
 import javafx.scene.control.PasswordField
 import javafx.scene.control.TextField
+import javafx.stage.Stage
 
 
 class HelloController {
@@ -28,10 +32,14 @@ class HelloController {
         dialogPane.stylesheets.add(
             this::class.java.getResource("/theme/dark-theme.css")?.toExternalForm()
         )
-        alert.title = "Log in correct"
-        alert.headerText = "Welcome ${user.username}"
-        alert.contentText = "User id: ${user.id}"
-        alert.showAndWait()
+
+        val loader = FXMLLoader()
+        loader.location = this::class.java.getResource("/views/main-view.fxml")
+        val root: Parent = loader.load()
+        val stage = Stage()
+        stage.scene = Scene(root)
+        stage.title = "MusOK Browser"
+        stage.show()
     }
 
 
--- src/main/kotlin/com/musok/musokdbbrowser/ui/controller/UserView.kt
+++ src/main/kotlin/com/musok/musokdbbrowser/ui/controller/UserView.kt
@@ -1,4 +1,18 @@
 package com.musok.musokdbbrowser.ui.controller
 
-class UserView {
+import com.musok.musokdbbrowser.api.connection.Server
+import com.musok.musokdbbrowser.api.mappings.user.User
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.Label
+import java.net.URL
+import java.util.*
+
+class UserView: Initializable {
+    @FXML private lateinit var usernamePlaceholder: Label
+    private lateinit var user: User
+    override fun initialize(p0: URL?, p1: ResourceBundle?) {
+        user = Server.getCurrentUser()
+        usernamePlaceholder.text = user.username
+    }
 }
