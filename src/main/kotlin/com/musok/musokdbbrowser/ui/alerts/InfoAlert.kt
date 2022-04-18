0107a8bcf7824799b0d5f8c3a0a8fc30e5516bf0
Xoquín Pérez Blanco <xoquinpb@wappingweb.com>
2022-04-18 19:36:36 +0200

    InfoAlert.kt

--- src/main/kotlin/com/musok/musokdbbrowser/ui/alerts/InfoAlert.kt
+++ src/main/kotlin/com/musok/musokdbbrowser/ui/alerts/InfoAlert.kt
@@ -1,4 +1,28 @@
 package com.musok.musokdbbrowser.ui.alerts
 
-class InfoAlert {
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.stage.Stage
+import java.util.*
+
+class InfoAlert(
+        alertName: String,
+        alertDesc: String
+): Stage() {
+    @FXML lateinit var lbAlertTitle: Label
+    @FXML lateinit var lbAlertDesc: Label
+
+    init {
+        val loader = FXMLLoader()
+        loader.location = this::class.java.getResource("/views/authenticate-view.fxml")
+        loader.resources = ResourceBundle.getBundle("bundles/strings", Locale.getDefault())
+        val root: Parent = loader.load()
+        this.scene = Scene(root)
+        this.scene.stylesheets.removeAll()
+        this.scene.stylesheets.add("/theme/dark-theme.css")
+        this.title = alertName
+    }
 }
