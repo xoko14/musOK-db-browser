934adf016cc046e5160493f371a07262e3a611ed
xoko14 <xoquinperezb@gmail.com>
2022-05-24 21:55:29 +0200

    missing files

---
+++ src/main/kotlin/com/musok/musokdbbrowser/ui/components/Enums.kt
@@ -0,0 +1,13 @@
+package com.musok.musokdbbrowser.ui.components
+
+enum class PlaybackStatus {
+    STOPPED,
+    PLAYING,
+    LOADING
+}
+
+enum class UploadStatus {
+    UPLOADING,
+    FAILED,
+    NOT_STARTED
+}
---
+++ src/main/kotlin/com/musok/musokdbbrowser/ui/model/song/upload/UploadInfo.kt
@@ -0,0 +1,7 @@
+package com.musok.musokdbbrowser.ui.model.song.upload
+
+data class UploadInfo(
+    val serverId: String,
+    val songId: Long,
+    val uploaderId: Long
+)
